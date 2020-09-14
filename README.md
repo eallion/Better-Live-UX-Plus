0709ec6cbf7955a3fc1c53e59d41268b97c223c0
Charles Chin <eallions@gmail.com>
2020-09-15 02:38:25 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# live-auto-web-fullscreen
+# Better-Live-UX-Plus
+Based on <https://greasyfork.org/zh-CN/scripts/404495-better-live-ux>
+
+# 让你拥有更好的直播体验！
+- 自动选择最高清晰度。
+- 自动禁止弹幕。
+- 自动禁止广告。
+- 网页全屏。
+
+# 支持：
+- 虎牙直播。
+- 斗鱼直播。
+- Bilibili 直播。
+- 企鹅电竞。
+- YY 直播。
+
+# 计划中：
+- 增加可配置项。
+- 支持（可选的）自动全屏或网页全屏。
+- 支持自动选择相应主播的牌子。
+- 自动领取箱子、完成任务。
---
+++ live-auto-web-fullscreen.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name           Better Live UX Plus
+// @name:zh-CN     更好的直播体验（最高清晰度、禁弹幕、禁广告、网页全屏）
+// @namespace      eallion
+// @version        2020.09.15
+// @description    自动选择最高清晰度、禁止弹幕、禁止广告、网页全屏。支持：斗鱼、虎牙、哔哩哔哩、企鹅电竞。
+// @author         eallion
+// @run-at         document-idle
+// @noframes
+// @require        https://cdn.jsdelivr.net/npm/jquery
+// @require        https://cdn.jsdelivr.net/gh/eallion/better-live-ux-plus@master/waitForKeyElements.js?version=6349
+// @match          *://live.bilibili.com/*
+// @match          *://www.douyu.com/*
+// @match          *://www.huya.com/*
+// @match          *://egame.qq.com/*
+// ==/UserScript==
+
+const config = {
+    huya: {
+        selectors: [
+            '#player-danmu-btn',
+            'ul.player-videotype-list > li:nth-child(1)',
+            'div.ab-close-btn',
+            '#player-fullpage-btn',
+        ],
+        timeout: 1500,
+    },
+    douyu: {
+        selectors: [
+            `div[class^='showdanmu-']`,
+            `div[class^='tip-'] > ul > li:nth-child(1)`,
+            `div[class^='wfs']:not([class^='wfs-exit'])`,
+        ],
+        timeout: 1500,
+    },
+    bilibili: {
+        selectors: [
+            'i.live-icon-danmaku-on',
+            'i.live-icon-web-fullscreen',
+            //'button[data-title="网页全屏"]',
+        ],
+        timeout: 1500,
+    },
+    qq: {
+        selectors: [
+            'div.vcp-extended-barrage',
+            'a.vcp-vertical-switcher-item-clarity:nth-child(1)',
+            'div.vcp-extended-webfullscreen',
+        ],
+        timeout: 1500,
+    },
+}
+
+const site = config[document.domain.split('.').reverse()[1]];
+
+(function() {
+    'use strict';
+
+    if (!site) {
+        return;
+    }
+
+    site.selectors.forEach(selector => {
+        setTimeout(() => {
+            waitForKeyElements(selector, (node) => {
+                node.click();
+            });
+        }, site.timeout || 0);
+    });
+})();
